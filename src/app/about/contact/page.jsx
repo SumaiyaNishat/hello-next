2b8bd19acfc2a1bc6c9cdae32f0b1560d422a273
SumaiyaNishat <sumaiyanishat94@gmail.com>
2026-04-17 12:12:56 +0600

    create pages and routes in next js

---
+++ src/app/about/contact/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/components/Title';
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <Title>My contact is 01311111111</Title>
+    </div>
+  )
+}
+
+export default Contact;
